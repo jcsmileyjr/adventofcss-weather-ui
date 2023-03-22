af3ef793c75600abb0e8275bcdc09f0dfc9875e9
Smiley <jcsmileyjr@gmail.com>
2023-03-22 12:45:24 -0500

    Refactor with bare bones structure of a section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section>
+          <p>WED</p>
+          <p>8</p>
+          <div>
+            <p></p>
+            <>
+              <div>
+                <p></p>
+              </div>
+              <div>
+                <p></p>
+              </div>
+            </>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
